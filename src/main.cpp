aede12414efedd3f2b4a94c67987a616bb8ef983
Yago Nascimento <yagovictorsilvaa@gmail.com>
2025-10-13 10:26:59 -0300

    Commit inicial do projeto

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
+}
+
+// put function definitions here:
