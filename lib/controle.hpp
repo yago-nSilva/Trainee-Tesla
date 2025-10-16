5a0128df5720ef03d738bba1768a56ccde7389c5
AqueleLaO <aquelelaooferreiro@gmail.com>
2025-10-16 16:09:14 -0300

    Add files via upload

---
+++ lib/controle.hpp
@@ -0,0 +1,7 @@
+void inicializar(const int RTDPin, const int MotorPin, const int NoisePin);
+
+void debounce(int state);
+
+void MudarEstado(int state);
+
+void AcionamentoMotor(const int MotorPin,  const int NoisePin);
