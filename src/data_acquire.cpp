b9e114c305a074a0011744fc466baced3e7af62e
Yago Nascimento <yagovictorsilvaa@gmail.com>
2025-10-19 00:19:32 -0300

    data acquire v1.0.1

--- src/data_acquire.cpp
+++ src/data_acquire.cpp
@@ -2,8 +2,9 @@
 
 #include<cmath>
 
-// Construtor
-SensorMPU::SensorMPU()
+// Classe SensorMPU
+
+SensorMPU::SensorMPU()  // Construtor
     : raw_acceleration({0,0,0}),
       filtered_acceleration({0,0,0}),
       acc_offset({0,0,0})
@@ -14,20 +15,23 @@ bool SensorMPU::initializeSensor(){
 
     // inicialização do sensor
     if (!mpu.begin()) {
+
         Serial.println("Falha ao encontrar chip MPU6050");
         while (1) {
-        delay(10);
+
+            delay(10);
+            return false;
         }
     }
 
-   // Range de medição em +- 2G 
-    mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-    mpu.setFilterBandwidth(MPU6050_BAND_10_HZ); // Recomendado para reduzir ruído
+    mpu.setAccelerometerRange(MPU6050_RANGE_2_G);   // Range de medição em +- 2G 
+    mpu.setFilterBandwidth(MPU6050_BAND_10_HZ);     // Recomendado para reduzir ruído
 
-    // Calibração do acelerômetro
+    // Sample Rate default = 1kHz
 
-    offsetCalibration();
+    offsetCalibration();    // Calibração do acelerômetro
 
+    return true;
 }
 
 void SensorMPU::offsetCalibration(){    // Determina qual o offset após fazer a média móvel
@@ -38,7 +42,7 @@ void SensorMPU::offsetCalibration(){    // Determina qual o offset após fazer a
         zAxis = 2
     };
 
-    const int nSamples = 100; // valor "alterável"
+    const int nSamples = 100; // valor de teste (pode ser alterado)
     float sum[3] = {0,0,0};
     float average[3] = {0,0,0};
     
@@ -52,13 +56,13 @@ void SensorMPU::offsetCalibration(){    // Determina qual o offset após fazer a
         sum[yAxis] += raw_acceleration.accy;
         sum[zAxis] += raw_acceleration.accz;
 
-        getRawAcceleration();
-
         delay(1); // teoricamente isso serve para não sobrecarregar a CPU
     }
 
     for (int i = 0; i < 3; i++){
+
         average[i] = sum[i] / nSamples;
+        delay(1);
     }
 
     acc_offset.accx = average[xAxis];
@@ -79,4 +83,75 @@ void SensorMPU::getRawAcceleration(){
 
 }
 
-// void SensorMPU::getFilteredAcc(){}
+float SensorMPU::getFilteredAcc(){
+
+    sensors_event_t raw_acc_measure;
+    const int nSamples = 3;  // Sample rate = 10hz --> 3 valores lidos = 333ms
+    
+    filtered_acceleration.accx = 0; // resetando filtered_accceleration.accx
+
+    for (int i = 0; i < nSamples; i++){
+
+        mpu.getEvent(&raw_acc_measure, nullptr, nullptr);  
+        filtered_acceleration.accx += (raw_acc_measure.acceleration.x - acc_offset.accx);   // O carrinho anda reto --> acc em apenas um eixo (necessário alterar no momento dos testes)
+        delay(1);
+    }
+
+    filtered_acceleration.accx = filtered_acceleration.accx / nSamples;
+
+    return filtered_acceleration.accx;
+
+}
+
+// Classe dataProcessing
+
+DataProcessing::DataProcessing(SensorMPU &sensor_aux)
+    : _sensor(sensor_aux),
+      instant_speed(0),
+      total_distance(0),
+      last_reading_time(0)
+{}
+
+float DataProcessing::getInstantSpeed(){
+
+    // falta armazenar e passar isso pra telemetria
+    return instant_speed;
+}
+
+void DataProcessing::updateSpeed(){
+
+    unsigned long current_time = millis();
+    const int ZERO_TOLERANCE = 0.05;
+
+    if (last_reading_time == 0){
+
+        last_reading_time = current_time; 
+        return;
+    }
+
+    float acceleration = _sensor.getFilteredAcc();
+    unsigned long delta_time = (current_time - last_reading_time) / 1000; // ms --> s
+
+    instant_speed += acceleration*delta_time;
+
+    last_reading_time = current_time;
+    // resolver problema pra velocidade quando a aceleração está caindo
+    // tentar com alguma comparação com o valor de aceleração anterior 
+    // atual < anterior --> carrinho desacelerando
+    // calcular a velocidade para ese caso 
+    // isso é chamado de drift --> como lidar? 
+}
+
+unsigned long DataProcessing::timeAccIsZero(){
+
+    float zero_acceleration_check;
+
+    const int ZERO_TOLERANCE = 0.05;
+
+    zero_acceleration_check = _sensor.getFilteredAcc();
+
+    if (std::abs(zero_acceleration_check) > ZERO_TOLERANCE)
+        return millis();
+
+    else return 0;
+}
--- src/data_acquire.hpp
+++ src/data_acquire.hpp
@@ -21,15 +21,36 @@
             Acceleration acc_offset; //importante para determinar a calibração 
             Adafruit_MPU6050 mpu;
 
-            void sensorCalibration();
-            void accelerationFilter();
+            void offsetCalibration();
+            void getRawAcceleration();
 
         public:
             SensorMPU();
-            Acceleration getFilteredAcc();
+          
+            float getFilteredAcc();         
             bool initializeSensor();
 
     };
 
+    class DataProcessing{
+        private:
+            SensorMPU &_sensor; // definição por composição
+
+            float instant_speed;
+            float total_distance;
+            
+            unsigned long last_reading_time;
+
+            unsigned long timeAccIsZero();
+
+        public:
+            DataProcessing(SensorMPU &sensor_aux):_sensor(sensor_aux){}
+
+            float getInstantSpeed();
+            void updateSpeed();
+
+            float getTotalDistance();
+
+    };
 
 #endif
