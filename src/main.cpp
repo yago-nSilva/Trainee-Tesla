6a8782d39a3dedbbe96355ac35d6e001629824a6
Yago Silva <yagovictorsilvaa@gmail.com>
2025-10-31 23:29:05 -0300

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,24 @@
+#include "data_acquire.hpp"
 #include <Arduino.h>
 
-// put function declarations here:
+SensorMPU mpu;
+
+DataProcessing data(mpu);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  // inicialização MPU6050
+  mpu.initializeSensor();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello World");
   delay(2000);
+
+  mpu.getFilteredAcc();
 }
 
 // put function definitions here:
